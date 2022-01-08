a723e67274db03e20e5861d8158b3753173ee024
Pawel <pawel.kuznik@gmail.com>
2022-01-08 11:23:21 +0100

    Added index

---
+++ cogi-409k.ts
@@ -0,0 +1,6 @@
+export { default as Detachment } from './src/Structures/Detachment';
+export { default as Keywords } from './src/Structures/Keywords';
+export { default as List } from './src/Structures/List';
+export { default as Unit } from './src/Structures/Unit';
+export { default as UnitDataSlate } from './src/Structures/UnitDataSlate';
+export { default as calculatePowerLevels } from './src/Actions/calculatePowerLevels';
