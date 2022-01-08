dc93f00c6649d41f3c86bdd2d412f48978f69b74
Pawel <pawel.kuznik@gmail.com>
2022-01-08 11:20:08 +0100

    Implemented calculatePowelLevels action + tests.

--- src/Actions/calculatePowerLevels.ts
+++ src/Actions/calculatePowerLevels.ts
@@ -1,13 +1,19 @@
 import Detachment from "../Structures/Detachment";
 import Unit from "../Structures/Unit";
 
+function isIterable(input:any) { return typeof input[Symbol.iterator] === 'function'; }
+
 function calculatePowerLevels(list: { detachments:Array<Detachment> }) : number;
 function calculatePowerLevels(detachment: { units:Array<Unit> }) : number;
 function calculatePowerLevels(units: Iterable<Unit>) : number;
 function calculatePowerLevels(input: any) : number {
 
-    if (typeof input[Symbol.iterator] === 'function') return [...input].reduce((prev:number, current:Unit) => prev += current.powerLevels, 0);
-    
+    if (input.detachments && isIterable(input.detachments)) return [...input.detachments].reduce((prev:number, current:Detachment) => prev + calculatePowerLevels(current), 0);
+
+    if (input.units && isIterable(input.units)) return calculatePowerLevels(input.units);
+
+    if (isIterable(input)) return [...input].reduce((prev:number, current:Unit) => prev + current.powerLevels, 0);
+
     return 0;
 };
 
--- tests/Actions/calculatePowerLevels.test.ts
+++ tests/Actions/calculatePowerLevels.test.ts
@@ -1,5 +1,7 @@
 import calculatePowerLevels from "../../src/Actions/calculatePowerLevels";
+import Detachment from "../../src/Structures/Detachment";
 import Keywords from "../../src/Structures/Keywords";
+import List from "../../src/Structures/List";
 import Unit from "../../src/Structures/Unit";
 import UnitDataSlate from "../../src/Structures/UnitDataSlate";
 
@@ -31,5 +33,48 @@ describe('calculatePowerLevels', () => {
         const powerLevels = calculatePowerLevels([ unitOne, unitTwo, unitThree ]);
 
         expect(powerLevels).toEqual(12);
+    });
+
+    test('it should calculate power levels of a detachment', () => {
+
+        const unitOne:Unit = { name: 'test-1', powerLevels: 4, dataSlate: dataSlateA };
+        const unitTwo:Unit = { name: 'test-2', powerLevels: 2, dataSlate: dataSlateA };
+        const unitThree:Unit = { name: 'test-3', powerLevels: 6, dataSlate: dataSlateA };
+
+        const detachment:Detachment = { units: [ unitOne, unitTwo, unitThree ] };
+
+        const powerLevels = calculatePowerLevels(detachment);
+
+        expect(powerLevels).toEqual(12);
+    });
+
+    test('it should calculate 0 power levels of empty', () => {
+
+        const detachment:Detachment = { units: [ ] };
+
+        const powerLevels = calculatePowerLevels(detachment);
+
+        expect(powerLevels).toEqual(0);
+    });
+
+    test('it should calculate power levels of a list', () => {
+
+        const unitAOne:Unit = { name: 'test-1', powerLevels: 4, dataSlate: dataSlateA };
+        const unitATwo:Unit = { name: 'test-2', powerLevels: 2, dataSlate: dataSlateA };
+        const unitAThree:Unit = { name: 'test-3', powerLevels: 6, dataSlate: dataSlateA };
+
+        const detachmentA:Detachment = { units: [ unitAOne, unitATwo, unitAThree ] };
+
+        const unitBOne:Unit = { name: 'test-1', powerLevels: 1, dataSlate: dataSlateA };
+        const unitBTwo:Unit = { name: 'test-2', powerLevels: 1, dataSlate: dataSlateA };
+        const unitBThree:Unit = { name: 'test-3', powerLevels: 1, dataSlate: dataSlateA };
+
+        const detachmentB:Detachment = { units: [ unitBOne, unitBTwo, unitBThree ] };
+
+        const list:List = { name: 'test-list', detachments: [ detachmentA, detachmentB ] };
+
+        const powerLevels = calculatePowerLevels(list);
+
+        expect(powerLevels).toEqual(15);
     });
 });
